1aa9602b2f9d0b832adc670a1c1b0ed6d2d06c98
bauzaite <66077990+bauzaite@users.noreply.github.com>
2022-05-30 21:53:44 +0100

    Typos

--- Help.bash
+++ Help.bash
@@ -1,5 +1,5 @@
 # Prerequisites:                          -    ╔══╗
-# mono                             -    ╚╗╔╝
+# mono                                    -    ╚╗╔╝
 # C# v7                                   -    ╔╝(¯`v´¯)
 # ctag gets confused with gtest syntax    -    ╚══`.¸.Defensive Programming
 
@@ -9,7 +9,7 @@ FIX_STRCPY=1
 INITALISE_ALL_VARIABLES=1
 CHECK_POINTER_NOT_NULL=1
 ADD_WINDOWS_STRCPY=1
-FIXES_IN_COMMENT=1 # do u want chnages applied directly or in a comment next to the incorrect code? 
+FIXES_IN_COMMENT=1 # would you like chanages applied directly or in a comment next to the incorrect code? 
 
 ctags --fields=+ne -o - --sort=yes $@ >> ctag.txt
 mono v2.exe $@ $FIX_STRCPY $INITALISE_ALL_VARIABLES $CHECK_POINTER_NOT_NULL $ADD_WINDOWS_STRCPY $FIXES_IN_COMMENT
