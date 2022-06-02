8078de2e9d5cb8b04c4507ab96484a5677f8fca0
bauzaite <66077990+bauzaite@users.noreply.github.com>
2022-06-02 11:14:45 +0100

    Main functionality of initParams is now finished
    Unit testing done, some more extensive testing left to do.
    
    Some TODOs left for future development but not required now.

--- checkParams.cs
+++ checkParams.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,119 +17,144 @@ namespace checkParams
 
         static List<parameter> allParams = new List<parameter>();
 
-        public static void notNull(functionInfo function, string functionHead)
-        {
-            // save variables if they have p in the name or ptr in type
-            initialiseParams(functionHead, allParams);
-
-            // remove any params in list that have been checked if they are NULL_PTR
-            foreach (string line in function.contents)
-            {
-                for (int i = 0; i < allParams.Count; i++) {
-                    if (line.Contains(allParams[i].name) && line.Contains("== NULL_PTR"))
-                    {
-                        allParams.RemoveAt(i);
-                    }
-                }
-            }
-
-            // if there are some params where their input isnt checked
-            if (allParams.Count > 0)
-            {
-                createParamsFix(function, allParams);
-            }
-
-            // ELSE DO NOTHING
-
-
-            // old notes
-            // if it has 'HANDLE' in the type then create 'parameter.name != CK_INVALID_HANDLE'
-            // if it has 'PTR' in the type then create  'parameter.name != null_ptr
-            // else then create 'parameter.nmae != 0'
-
-            // 1) create a code line that checks all these and put it in the code
-            // fix = 
-            // if(  )
-
-
-            // 2) code line that only contains params checks that dont already esxist
-
-        }
-
         /// <summary>
-        /// take all pointer parameters and create a fix to put into the code
+        /// take the 'head' of function and extract the appriate params and put them into a list
         /// </summary>
-        /// <param name="function"></param>
-        /// <param name="parameterList"></param>
-        static void createParamsFix(functionInfo function, List<parameter> parameterList)
-        {
-            // create fix and add to top of the file
-
-            int firstInstIf = 0;
-
-            for (int i = 0; i < function.length; i++)
-            {
-                if (function.contents[i].Contains("if (")) // add a catch here: they might of not have any if statements
-                {
-                    firstInstIf = function.start + i;
-                    break;
-                }
-            }
-
-            List<string> paramFix = new List<string>();
-            string paramNames = "";
-
-
-            foreach (parameter param in parameterList)
-            {
-                paramFix.Add(param.name + " == NULL_PTR");   // theres gonna be a bug-ish here. what if teres only one ptr param it wont look right in error msg
-                paramNames = paramNames + param.name + " ";
-            }
-            string newFix = "if (" ;
-
-            foreach(string pp in paramFix){
-                newFix = newFix + pp;
-            }
-
-            newFix = newFix + "){";
-
-            string[] paramFixArray = {newFix, "{", "rv = CKR_ARGUMENTS_BAD", "WCK_ERROR(\""+function.name+ " "+ paramNames + " is NULL\")", "}" };
-
-            Utilities.all.applyFixaRRAY(paramFixArray, @"C:\Users\bauzaiv\testData.txt", firstInstIf - 1);
-
-        }
-
-        /// <summary>
-        /// take the 'head' of function and extract the params and put them into a list
-        /// </summary>
-        /// <param name="functionHead"></param>
-        /// <param name="parameterList"></param>
-        static void initialiseParams(string functionHead, List<parameter> parameterList)
+        /// <param name="function">Function that would be called by customer</param>
+        /// <param name="functionHead">Function name and parameters</param>
+        /// <param name="parameterList">Ponter parameters that have been sent by the customer</param>
+        static void initialiseParams(functionInfo function, string functionHead, List<parameter> parameterList)
         {
             string allParams = functionHead.Split('(')[1];
+            parameter currentParameter = new parameter();
             allParams = allParams.Split(')')[0];
-
             List<string> paramList = allParams.Split(',').ToList<string>();
-            for (int i = 0; i < paramList.Count; i++)
+
+            // trim each parameter
+            for (int x = 0; x < paramList.Count; x++)
             {
-                paramList[i] = paramList[i].Trim();
+                paramList[x] = paramList[x].Trim();
             }
 
-            string[,] sepPrams = new string [paramList.Count(), 2] ;
-            int k = 0;
-            parameter currentParameter = new parameter();
+            // remove all params that are not pointers
             for (int j = 0; j < paramList.Count(); j++)
             {
                 string[] currentChars = paramList[j].Split(' ');
                 currentParameter.type = currentChars[0];
                 currentParameter.name = currentChars[1];
-                if (currentParameter.name.Contains("p") || currentParameter.type.Contains("PTR")) {
+                if (currentParameter.name.Contains("p") || currentParameter.type.Contains("PTR"))
+                {
                     parameterList.Add(currentParameter);
                 }
                 currentParameter = default(parameter);
             }
+
+            // remove all params from list that have been checked if they are null_ptr
+            foreach (string line in function.contents)
+            {
+                for (int i = 0; i < parameterList.Count; i++)
+                {
+                    if (line.Contains("if") && line.Contains(parameterList[i].name) && line.Contains("== NULL_PTR"))
+                    {
+                        parameterList.RemoveAt(i);
+                    }
+                }
+            }
         }
 
+        /// <summary>
+        /// take all pointer parameters and create a fix to put into the code
+        /// </summary>
+        /// <param name="function">Function that would be called by customer</param>
+        /// <param name="parameterList">Pointer parameters sent by the customer, that we have not checked if they are NULL_PTR</param>
+        static void createParamsFix(functionInfo function, List<parameter> parameterList)
+        {
+            int firstInstIf = 0;
+            string fixFirstInsIf = "error";
+            List<string> uncheckedParams = new List<string>();
+            string uncheckedParamNames = "error";
+            string tab = "  ";
+            string uncheckedParamsIfLine = "error";
+            string[] result;
+
+            // Find the correct location of the functions to put the new check in
+            for (int i = 0; i < function.length; i++)
+            {
+                if (function.contents[i].Contains("if (")) // add a catch here: they might of not have any if statements
+                {
+                    firstInstIf = function.start + i;
+                    fixFirstInsIf = function.contents[i].Replace("if", "else if");
+                    break;
+                }
+                else if (function.length == i)
+                {
+                    // they have no ifs, put check at top of the function
+                    firstInstIf = function.start + 1; // TODO: put below functionhead 
+                    fixFirstInsIf = "else {}"; // TEST THIS!
+                }
+            }
+
+            // For the logging line: Save the params that were not checked
+            foreach (parameter param in parameterList)
+            {
+                uncheckedParams.Add(param.name + " == NULL_PTR");   // theres gonna be a bug-ish here. what if teres only one ptr param it wont look right in error msg
+                uncheckedParamNames = uncheckedParamNames + param.name;
+            }
+
+            // Prep the first like of if loop
+            uncheckedParamsIfLine = "if (";
+            foreach (string parameter in uncheckedParams)
+            {
+                uncheckedParamsIfLine = uncheckedParamsIfLine + parameter;
+            }
+            uncheckedParamsIfLine = uncheckedParamsIfLine + "){";
+
+            // Create array to apply into the file.
+            if (Start.Start.changesCommented == 1)
+            {
+                result = new string[]{ "// ☠" + tab + uncheckedParamsIfLine,
+                                               "// ☠" + tab + "{",
+                                               "// ☠" + tab + tab + "rv = CKR_ARGUMENTS_BAD",
+                                               "// ☠" + tab + tab + "WCK_ERROR(\"" + function.name + " " + uncheckedParamNames + " is NULL\")",
+                                               "// ☠" + tab + "}",
+                                               "// ☠" + fixFirstInsIf
+                };
+            }
+            else
+            {
+                result = new string[] { tab + uncheckedParamsIfLine + " // ☠",
+                                               tab + "{ // ☠",
+                                               tab + tab + "rv = CKR_ARGUMENTS_BAD // ☠",
+                                               tab + tab + "WCK_ERROR(\"" + function.name + " " + uncheckedParamNames + " is NULL\") // ☠",
+                                               tab + "} // ☠",
+                                               fixFirstInsIf + " // ☠"
+                };
+            }
+
+            // Apply change into the file.
+            Utilities.all.applyFixArray(result, @"C:\Users\bauzaiv\testData.txt", firstInstIf);
+        }
+
+        /// <summary>
+        /// If there are sent pointer parameters, check the function ensures they
+        /// are not NULL_PTR before doing anything else.
+        /// </summary>
+        /// <param name="function">Functioned called by customer</param>
+        /// <param name="functionHead">Part of function that contains its name and parameters sent to it</param>
+        public static void notNull(functionInfo function, string functionHead)
+        {
+            initialiseParams(function, functionHead, allParams);
+
+            // If there are some params where their input isnt checked
+            if (allParams.Count > 0)
+            {
+                createParamsFix(function, allParams);
+            }
+            // TODO:
+            // like we do - if it has 'PTR' in the type then create  'parameter.name != null_ptr 
+            // make a new - if it has 'HANDLE' in the type then create 'parameter.name != CK_INVALID_HANDLE' check
+
+        }
 
     }
 }
