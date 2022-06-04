451125f43791460a8b0579592cc32b559b807dca
bauzaite <66077990+bauzaite@users.noreply.github.com>
2022-06-04 20:48:10 +0100

    Updated Decriptions

--- checkStrcpy.cs
+++ checkStrcpy.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,8 +9,20 @@ using System.IO;
 
 namespace fixStrings
 {
+    /// <summary>
+    /// This class ensures the user is using strcpy() functon safely.
+    /// Otherwise, it suggest to either improve strcpy() and gives fixes
+    /// or it suggests to use strncpy() and gives a fix.
+    /// </summary>
     public class checkStrcpy
     {
+        /// <summary>
+        /// Figure out if use of strcpy() is safe, otherwise
+        /// suggest to the user to use strncpy() instead.
+        /// </summary>
+        /// <param name="function">function informaton that contains strcpy</param>
+        /// <param name="fixLine">the line in te function with 'strcoy' in it</param>
+        /// <param name="location">the location of the 'strcpy' mention</param>
         public static void strCpyUsed(functionInfo function, string fixLine, int location)
         {
             string desinationName = fixLine.Split('(', ',')[1].Trim();
@@ -29,8 +41,8 @@ namespace fixStrings
 
                         if (source.Length + 1 >= sizeOfDestination)
                         {
-                            string fix = function.contents[line].Replace(sizeOfDestinationStr, (source.Length + 1).ToString());
-                            Utilities.all.applyFix(fix, Start.Start.fileLocation, function.start + line);
+                            string fix = "// ☠" + function.contents[line].Replace(sizeOfDestinationStr, (source.Length + 1).ToString());
+                            Utilities.all.applySingleLineFix(function, fix, function.start + line);
                         }
                         break;
                     }
@@ -51,18 +63,22 @@ namespace fixStrings
             }
         }
 
+        /// <summary>
+        /// Check if source of strcpy(dst,src) is defined locally.
+        /// </summary>
+        /// <param name="function">contents of the function</param>
+        /// <param name="source">the name of source in the function</param>
+        /// <returns></returns>
         static bool SourceDefined(functionInfo function, ref string source)
         {
-            if (source.Contains("\""))
+            if (source.Contains("\"")) // if source has been defined like this strcpy(dst, "hello")
             {
-                source = source.Split("\"")[1];
-                //source = source.Split("\"")[0];
-                // now it looks like thiis -> source = "hello"
+                source = source.Split("\"")[1]; // format it and return it like so -> source = "hello"
                 return true;
             }
-            else
-            {   // this means source contains name of source
-                for (int line = 0; line < function.length; line++)
+            else // this means source is a initialised variable -> strcpy(dst, hello)
+            {   
+                for (int line = 0; line < function.length; line++) // look if it's been definied locally
                 {
                     if (function.contents[line].Contains(source) && function.contents[line].Contains("="))
                     {
@@ -80,6 +96,12 @@ namespace fixStrings
             return false;
         }
 
+        /// <summary>
+        /// Suggest the user to user strncpy instead and give a semi-automated fix.
+        /// </summary>
+        /// <param name="function">function with strcpy</param>
+        /// <param name="strCpyLine">line containing the strcpy</param>
+        /// <param name="location">location of function strcpy is menttioned in</param>
         static void StrcpyToStrncpy(functionInfo function, string strCpyLine, int location)
         {
             string desinationName = strCpyLine.Split('(', ',')[1].Trim();
@@ -88,14 +110,13 @@ namespace fixStrings
 
             if (Start.Start.changesCommented == 1) {
                 fix = strCpyLine + " // ☠ strncpy(" + desinationName +
-                       ", " + sourceName + ", /* sizeof(" + desinationName + ")*/);";
+                      ", " + sourceName + ", /* sizeof(" + desinationName + ")*/);";
             }
             else {
                 fix = strCpyLine.Split('s')[0] + "strncpy(" + desinationName +
-                    ", " + sourceName + ", /* strlen(" + desinationName + ")*/); // ☠";
+                      ", " + sourceName + ", /* strlen(" + desinationName + ")*/); // ☠";
             }
-            Utilities.all.applyFix(fix, Start.Start.fileLocation, function.start + location);
-            Utilities.all.updateStructure(function, fix, location);
+            Utilities.all.applySingleLineFix(function, fix, function.start + location);
         }
     }
 }
