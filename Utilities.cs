a41e04b771fc858e0a29f817278b0a86c7dd5707
bauzaite <66077990+bauzaite@users.noreply.github.com>
2022-06-04 20:49:45 +0100

    Tidied file and added new functionalities

--- Utilities.cs
+++ Utilities.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,36 +10,87 @@ namespace Utilities
     public class all
     {
         /// <summary>
-        /// Adds a fix into the line specified
+        /// Adds a fix into the line specified.
         /// </summary>
-        /// <param name="newLine"></param>
-        /// <param name="fileName"></param>
-        /// <param name="lineNumber"></param>
-        public static void applyFix(string newLine, string fileName, int lineNumber)
+        /// <param name="newLine">The new line to add into the users file</param>
+        /// <param name="lineNumber">Which line in the file to apply the change to</param>
+        public static void applySingleLineFix(functionInfo function, string newLine, int lineNumber)
         {
             // TODO: Use streamwriter?
-            string[] wholeFile = File.ReadAllLines(fileName);
+            string[] wholeFile = File.ReadAllLines(Start.Start.fileLocation);
             wholeFile[lineNumber] = newLine;
-            File.WriteAllLines(fileName, wholeFile);
-        }
-        public static void updateStructure(functionInfo function, string newLine, int location)
-        {
-            function.contents[location] = newLine; // this may not be needed, test it
+            File.WriteAllLines(Start.Start.fileLocation, wholeFile);
+
+            updateSingleLineInStructure(function, newLine, lineNumber - function.start);
         }
 
         /// <summary>
-        /// Adds a array of new lines below the line specified
+        /// Adds an array of new lines below the line specified.
         /// </summary>
         /// <param name="newLine">set of new lines to insert into the file</param>
-        /// <param name="fileName"></param>
         /// <param name="lineNumber">under which line to insert changes to</param>
-        public static void applyFixArray(string[] newLine, string fileName, int lineNumber)
+        public static void applyMultipleLineFix(functionInfo function, string[] newLines, int lineNumber)
         {
             // TODO: Use streamwriter?
-            // i think line number was wrong
-            List<string> wholeFile = new List<string>(File.ReadAllLines(fileName));
-            wholeFile.InsertRange(lineNumber, newLine);
-            File.WriteAllLines(fileName, wholeFile);
+            List<string> wholeFile = new List<string>(File.ReadAllLines(Start.Start.fileLocation));
+            wholeFile.InsertRange(lineNumber, newLines);
+            File.WriteAllLines(Start.Start.fileLocation, wholeFile); 
+            //clean up
+            wholeFile = new List<string>();
+
+            updateMultiplesLineInStructure(function, newLines, lineNumber - function.start);
+        }
+
+        /// <summary>
+        /// Since we have updated how the users file looks, we need to update how
+        /// our main allFunctions structure looks aswell.
+        /// Update a single line in a single functions contents.
+        /// TODO: This is not the most elegant way to update our structure. Think of other ways.
+        /// </summary>
+        /// <param name="function">The function that has been edited</param>
+        /// <param name="newLine">The line in the function that has been edited</param>
+        /// <param name="location">The location of the line that has been edited</param>
+        public static void updateSingleLineInStructure(functionInfo function, string newLine, int location)
+        {
+            function.contents[location] = newLine;
+        }
+
+        /// <summary>
+        /// Since we have updated how the users file looks, we need to update how
+        /// our main allFunctions structure looks aswell.
+        /// Update multiple lines in a single functions contents. Then update the location
+        /// of where the function starts and ends in a file. Finally, update each function that
+        /// has been affected by this change.
+        /// </summary>
+        /// <param name="function">The function that has been edited</param>
+        /// <param name="newLines">The line in the function that has been edited</param>
+        /// <param name="location">The location of the line that has been edited</param>
+        public static void updateMultiplesLineInStructure(functionInfo function, string[] newLines, int location)
+        {
+            int oldEnd = function.end;
+            // update the functions contents
+            for (int i = 0; i < newLines.Length; i++)
+            {
+                function.contents.Insert(location + i, newLines[i]);
+            }
+
+            function.end = function.end + newLines.Length;
+            // update the locations of where function starts and ends, and all functions below it.
+            functionInfo tempInfo = new functionInfo();
+            for (int i = 0; i < Start.Start.allFunctions.Count; i++)
+            {
+                // Ctags does not name the functions inside a file in order so our allFunctions
+                // structure pretty much has the functions in a random order.
+                if (Start.Start.allFunctions[i].start > oldEnd)
+                {
+                    tempInfo = Start.Start.allFunctions[i];
+                    tempInfo.start = tempInfo.start + newLines.Length;
+                    tempInfo.end = tempInfo.end + newLines.Length;
+
+                    Start.Start.allFunctions[i] = tempInfo;
+                    tempInfo = default(functionInfo);
+                }
+            }
         }
 
         /// <summary>
